3514d54b3ecb56b235be22eafee18aa2ed54d1e3
jonny <beingswanand@gmail.com>
2020-07-12 22:25:16 +0530

    changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import about from '../views/Aboutus.vue'
 
 Vue.use(VueRouter)
 
@@ -11,14 +10,7 @@ Vue.use(VueRouter)
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: about
-  }
+ 
 ]
 
 const router = new VueRouter({
